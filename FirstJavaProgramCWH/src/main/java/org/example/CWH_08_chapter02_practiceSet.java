b7bef0e6a43beab69188d6566fb454ee1560f9f7
this-is-emon <mofijul.islam.emon@gmail.com>
2024-08-01 11:59:51 +0600

    chapter-02, practice set done

--- FirstJavaProgramCWH/src/main/java/org/example/CWH_08_chapter02_practiceSet.java
+++ FirstJavaProgramCWH/src/main/java/org/example/CWH_08_chapter02_practiceSet.java
@@ -1,2 +1,37 @@
-package org.example;public class CWH_08_chapter02_practiceSet {
+package org.example;
+import java.util.Scanner;
+public class CWH_08_chapter02_practiceSet {
+    public static void main(String[] args) {
+        /* ----- Q-02 : What will be the result of the following expression? float a = ((7/4) * (9/2));  ----- */
+//        float a = ((7/4)*(9/2));
+//        System.out.println(a);
+        /* ----- Q-03 : Write a Java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade ----- */
+//        Scanner input = new Scanner(System.in);
+//        int numberInput = input.nextInt();
+//        int encryptedInput = numberInput + 8;
+//        System.out.println("Decrypted value" + (encryptedInput-8));
+        /* ----- Q-04 : Use comparison operators to find out whether a given number is greater than the user intended number or not ----- */
+//        int givenNumber = 45;
+//        Scanner input = new Scanner(System.in);
+//        int userIntendedNumber = input.nextInt();
+//        if (userIntendedNumber > givenNumber) {
+//            System.out.println("User intended number is GREATER");
+//        }else {
+//            System.out.println("User intended number is LESS");
+//        }
+
+        /* ----- Q-05 : Write the following expression in a Java program : ((v*v)-(u*u))/(2*q*s) ----- */
+//        double v = 67.89;
+//        double u = 89.56;
+//        double q = 75.56;
+//        double s = 94.56;
+//        double expression = ((v*v)-(u*u))/(2*q*s);
+//
+//        System.out.println(expression);
+
+        /* ----- Q-06 : Find the value of the following expression : int x=7; int a=7.49/7 + 35/7; What is the value of a?  ----- */
+        int x = 7;
+        double a = (7.49/7) + (35/7);
+        System.out.println(a);
+    }
 }
