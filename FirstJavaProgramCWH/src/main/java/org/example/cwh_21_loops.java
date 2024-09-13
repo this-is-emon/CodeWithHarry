6db6abb61818c86ddece03b0390cfe366cf3e4eb
this-is-emon <mofijul.islam.emon@gmail.com>
2024-09-13 12:07:53 +0600

    Loops theory in Java done

---
+++ FirstJavaProgramCWH/src/main/java/org/example/cwh_21_loops.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class cwh_21_loops {
+    public static void main(String[] args) {
+        /**-- Quick Quiz 01 : Write a program to print natural numbers from 100 to 200. --**/
+//        int naturalNumber = 100;
+//        while (naturalNumber <= 200){
+//            System.out.println(naturalNumber);
+//            naturalNumber++;
+//        }
+        System.out.println("-----------------------------------");
+
+        /**-- Quick Quiz 02 : Write a program to print first n natural numbers using 'do-while' loop? --**/
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Write the last number you want to print : ");
+//        int n = scanner.nextInt();
+//        int num = 1;
+//        do{
+//            System.out.println(num);
+//            num++;
+//        }while (num <= n);
+        System.out.println("-----------------------------------");
+
+        /**-- Quick Quiz 03 : Write a program to print first n natural numbers in reverse order --**/
+
+        for (int i = 10; i > 0 ; i--) {
+            System.out.println(i);
+        }
+        System.out.println("-----------------------------------");
+
+        /**-- CONCEPT :
+         * "Break" statement completely exits the loop.
+         * "Continue" statement skips the particular iteration of the loop.
+         * --**/
+        System.out.println("-----------------------------------");
+
+
+    }
+}
