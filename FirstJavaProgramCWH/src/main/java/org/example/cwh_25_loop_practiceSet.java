280e8e76d0ac31ee3f4394b9259e7382adecfe79
this-is-emon <mofijul.islam.emon@gmail.com>
2024-09-14 11:54:06 +0600

    Loop practice set done

---
+++ FirstJavaProgramCWH/src/main/java/org/example/cwh_25_loop_practiceSet.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class cwh_25_loop_practiceSet {
+    public static void main(String[] args) {
+        /**--01. Write a program to print the following pattern
+
+         ****
+         ***
+         **
+         *
+         --**/
+//        for (int i = 4; i > 0 ; i--) {
+//            for (int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        System.out.println("------------------------------------");
+
+        /**--02. Write a program to sum first 'n' even numbers using while loop--**/
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the value of n : ");
+//        int n = scanner.nextInt();
+//        int firstEvenNum = 2;
+//        while (firstEvenNum <= n){
+//            System.out.println(firstEvenNum);
+//            firstEvenNum = firstEvenNum + 2;
+//        }
+        System.out.println("-------------------------------------");
+
+        /**--03. Write a program to print multiplication table of a given number 'n'.--**/
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Write the value of n : ");
+//        int n = scanner.nextInt();
+//        for (int i = 1; i < 11 ; i++) {
+//            System.out.println(n + " * " + i + " = " + n*i);
+//        }
+        System.out.println("-------------------------------------");
+
+        /**--04. Write a program to print multiplication table of 10 in reverse order--**/
+//        for (int i = 10; i > 0 ; i--) {
+//            System.out.println("10 * " + i + " = " + 10*i);
+//        }
+        System.out.println("-------------------------------------");
+
+        /**--05. Write a program to find factorial of a given number using for loops --**/
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Write factorial number n : ");
+//        int n = scanner.nextInt();
+//        int factorialNum = 1;
+//        for (int i = n; i > 0 ; i--) {
+//            factorialNum = factorialNum*i;
+//        }
+//        System.out.println("Factorial of " + n + " is " + factorialNum);
+
+        System.out.println("-------------------------------------");
+
+        /**--06. Repeat 5 using while loop--**/
+//        int i = 0;
+//        while(i < 5){
+//            System.out.println("5");
+//            i++;
+//        }
+//        System.out.println("-------------------------------------");
+
+        /**--07. Repeat 1 using for/while loop--**/
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("1");
+//        }
+        System.out.println("-------------------------------------");
+
+        /**--08. What can be done using one type of loop can also be done using the other two types of loops - True or false--**/
+//        System.out.println("True.");
+        System.out.println("-------------------------------------");
+
+        /**--09. Write a program to calculate the sum of the numbers occurring in the multiplication table of 8--**/
+//        int sum = 0;
+//        for (int i = 1; i < 11 ; i++) {
+//            sum = sum + (i*8);
+//        }
+//        System.out.println("Sum of the numbers occurring in the multiplication table of 8 : " + sum);
+        System.out.println("-------------------------------------");
+
+        /**--10. A do-while loop is executed :
+         * at least once
+         * at least twice
+         * at most once
+         * --**/
+//        System.out.println("At least Once.");
+        System.out.println("-------------------------------------");
+
+        /**--11. Repeat 2 using for loop--**/
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("2");
+//        }
+        System.out.println("-------------------------------------");
+
+    }
+}
